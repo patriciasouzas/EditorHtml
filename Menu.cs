4b09e384f6df2a3c78d18fae18510c7efa7432ca
patriciasouzas <patriciasouzaa89@gmail.com>
2023-04-10 20:11:50 -0300

    adicionados novos métodos

--- Menu.cs
+++ Menu.cs
@@ -9,10 +9,12 @@ namespace EditorHtml
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Black;
+
             DrawScreen();
             WriteOptions();
 
             var option = short.Parse(Console.ReadLine());
+            HandleMenuOption(option);
         }
 
         public static void DrawScreen()
@@ -58,6 +60,22 @@ namespace EditorHtml
             Console.WriteLine("0 - Sair");
             Console.SetCursorPosition(3, 10);
             Console.Write("Opção: ");
+        }
+
+        public static void HandleMenuOption(short option)
+        {
+            switch (option)
+            {
+                case 1: Console.WriteLine("Editor"); break;
+                case 2: Console.WriteLine("View"); break;
+                case 0:
+                    {
+                        Console.Clear();
+                        Environment.Exit(0);
+                        break;
+                    }
+                default: Show(); break;
+            }
         }
 
     }
